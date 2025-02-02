707a0a91b2ac5e6dc89e7ef04577bb1d14f17ad1
leoneltrich <leoneltrich@icloud.com>
2025-02-02 19:45:57 +0100

    Fine tuned designs and added state hook to FilterCollection.tsx hook is passed to the button to enable tracking of applied filters

--- eyecan-frontend/src/components/dashboard/sidebar/filter-collection/FilterPreviewCard.tsx
+++ eyecan-frontend/src/components/dashboard/sidebar/filter-collection/FilterPreviewCard.tsx
@@ -2,33 +2,40 @@ import {Card, Heading, HStack} from "@chakra-ui/react";
 import ApprovalIcon from "@/components/miscellaneous/ApprovalIcon.tsx";
 import UnitsCard from "@/components/dashboard/sidebar/filter-collection/UnitsCard.tsx";
 import ApplyFilterButton from "@/components/miscellaneous/apply-filter-button/ApplyFilterButton.tsx";
+import {Filter} from "@/types/filter.ts";
 
 interface FilterPreviewCardProps {
-    title: string;
-    text: string;
-    filterUnits: string[];
+    filter: Filter;
     datasetUnits: string[];
     textPreviewLength?: number;
+    appliedFiltersIds: number[];
+    onApplyFilter: (filterId: number) => void;
 }
 
-function FilterPreviewCard({title, text, filterUnits, datasetUnits, textPreviewLength = 180}: FilterPreviewCardProps) {
+function FilterPreviewCard({filter, datasetUnits, textPreviewLength = 180, appliedFiltersIds, onApplyFilter}: FilterPreviewCardProps) {
 
-    const allUnitsIncluded = filterUnits.every(unit => datasetUnits.includes(unit));
+    const allUnitsIncluded = filter.units.every(unit => datasetUnits.includes(unit));
 
     return (
-        <Card.Root marginTop={4} backgroundColor="transparent" border="none" >
+        <Card.Root marginTop={4} backgroundColor="transparent" border="none">
             <Card.Header padding={0}>
                 <HStack justifyContent="space-between" alignItems="center">
-                    <Heading margin={0} fontSize="xs" fontWeight="800">{title}</Heading>
+                    <Heading margin={0} fontSize="xs" fontWeight="800">{filter.title}</Heading>
                     <ApprovalIcon approved={allUnitsIncluded} color={"#808080"}/>
                 </HStack>
             </Card.Header>
             <Card.Body lineHeight={1.1} fontSize={10} color="#808080" padding={0}>
-                {text.length > textPreviewLength ? text.slice(0, textPreviewLength) + "..." : text}
+                {filter.description.length > textPreviewLength ? filter.description.slice(0, textPreviewLength) + "..." : filter.description}
             </Card.Body>
             <Card.Footer padding={0} paddingTop={2} justifyContent="space-between">
-                <UnitsCard text="Filter Units" units={filterUnits} />
-                <ApplyFilterButton margins={{marginTop: 10}}/>
+                <UnitsCard text="Filter Units" units={filter.units}/>
+                <ApplyFilterButton
+                    disabled={!allUnitsIncluded}
+                    applicable={allUnitsIncluded}
+                    applied={appliedFiltersIds.includes(filter.id)}
+                    margins={{marginTop: 10}}
+                    onClick={() => onApplyFilter(filter.id)}
+                />
             </Card.Footer>
         </Card.Root>
     )
--- eyecan-frontend/src/components/miscellaneous/apply-filter-button/ApplyFilterButton.tsx
+++ eyecan-frontend/src/components/miscellaneous/apply-filter-button/ApplyFilterButton.tsx
@@ -2,20 +2,25 @@ import styles from "./applyFilterButton.module.css"
 import {Margins} from "@/types/spacing.ts";
 
 interface ApplyFilterButtonProps {
+    applied?: boolean;
+    applicable: boolean; // TODO Make this own interface
+    disabled?: boolean;
     margins?: Margins;
+    onClick?: () => void;
 }
 
-function ApplyFilterButton({margins}: ApplyFilterButtonProps) {
+function ApplyFilterButton({margins, applied, applicable, disabled, onClick}: ApplyFilterButtonProps) {
     return (
-        <button style={{
+        <button disabled={disabled} style={{
             marginTop: `${margins?.marginTop}px`,
             marginBottom: `${margins?.marginBottom}px`,
             marginLeft: `${margins?.marginLeft}px`,
             marginRight: `${margins?.marginRight}px`,
         }}
                 className={styles.applyFilterButton}
+                onClick={onClick}
         >
-            Not Applicable
+            {applicable ? applied ? "Applied" : "Not Applied" : "Not Applicable"}
         </button>
     )
 }
