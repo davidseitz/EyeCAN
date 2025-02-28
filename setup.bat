26d7a6691ca2fe05dcf34b7cb205b0ad261e079c
supermalzi <robin-malzbender@web.de>
2025-02-28 16:41:48 +0100

    Fixed build and partially working setup.bat

--- setup.bat
+++ setup.bat
@@ -7,14 +7,24 @@ git submodule update --init --recursive
 
 set SVPWD=%CD%
 set CMAKE_PATH="C:\Program Files\JetBrains\CLion 2024.3.1.1\bin\cmake\win\x64\bin\cmake.exe"
+set MINGW_PATH=C:\MinGW\bin
+
+REM Add MinGW to PATH
+set PATH=%MINGW_PATH%;%PATH%
 
 REM Build dbcppp
 cd libraries\dbcppp
-if not exist build mkdir build
-cd build
-%CMAKE_PATH% -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\.local -DCMAKE_BUILD_TYPE=Release ..
-%CMAKE_PATH% --build . --config Release
-%CMAKE_PATH% --build . --config Release --target install
+if exist build (
+    cd build
+    del /q CMakeCache.txt
+    rmdir /s /q CMakeFiles
+) else (
+    mkdir build
+    cd build
+)
+%CMAKE_PATH% -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\.local -DCMAKE_BUILD_TYPE=Release ..
+mingw32-make
+mingw32-make install
 
 REM Build zlib
 cd /d %TEMP%
@@ -24,17 +34,31 @@ if not exist zlib-1.3.1 (
   powershell -Command "tar -xf zlib-1.3.1.tar.gz"
 )
 cd zlib-1.3.1
-%CMAKE_PATH% -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\.local -DCMAKE_C_FLAGS=-fPIC -DBUILD_SHARED_LIBS=OFF .
-%CMAKE_PATH% --build . --config Release
-%CMAKE_PATH% --build . --config Release --target install
+if exist build (
+    cd build
+    del /q CMakeCache.txt
+    rmdir /s /q CMakeFiles
+) else (
+    mkdir build
+    cd build
+)
+%CMAKE_PATH% -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\.local -DCMAKE_C_FLAGS=-fPIC -DBUILD_SHARED_LIBS=OFF ..
+mingw32-make
+mingw32-make install
 
 REM Build mdflib
 cd /d %SVPWD%\libraries\mdflib
-if not exist build mkdir build
-cd build
-%CMAKE_PATH% -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\.local -DZLIB_LIBRARY=%USERPROFILE%\.local\lib\zlib.lib -DZLIB_INCLUDE_DIR=%USERPROFILE%\.local\include -DCMAKE_BUILD_TYPE=Release ..
-%CMAKE_PATH% --build . --config Release
-%CMAKE_PATH% --build . --config Release --target install
+if exist build (
+    cd build
+    del /q CMakeCache.txt
+    rmdir /s /q CMakeFiles
+) else (
+    mkdir build
+    cd build
+)
+%CMAKE_PATH% -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\.local -DZLIB_LIBRARY=%USERPROFILE%\.local\lib\libz.a -DZLIB_INCLUDE_DIR=%USERPROFILE%\.local\include -DCMAKE_BUILD_TYPE=Release ..
+mingw32-make
+mingw32-make install
 
 cd /d %SVPWD%
 
