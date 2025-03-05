7ae87454ed3f52db4f2661c3d1f61d1bd99408ed
supermalzi <robin-malzbender@web.de>
2025-03-05 16:39:39 +0100

    creation of local EyeCAN directory

--- include/informationHandler.h
+++ include/informationHandler.h
@@ -14,13 +14,13 @@ using json = nlohmann::ordered_json;
 class InformationHandler {
 public:
   InformationHandler();
-  ~InformationHandler();
-  virtual json create(json info);
-  virtual json edit(json info, std::string id);
+  virtual ~InformationHandler() = default;
+  virtual int create(json& info) = 0;
+  virtual int edit(json& info, std::string id);
   virtual int remove(std::string id);
   virtual std::list<json> get(int page);
 protected:
-  std::string localKnowledgebase;
+  std::string localEyeCANPath;
 };
 
 #endif //INFORMATIONHANDLER_H
