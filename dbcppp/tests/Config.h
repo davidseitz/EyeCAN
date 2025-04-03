57f10f4fd53fd4b5eb808741900e4d0ad3c00bf5
supermalzi <robin-malzbender@web.de>
2025-04-03 19:13:19 +0200

    Added dist folder

--- dbcppp/tests/Config.h
+++
@@ -1,3 +0,0 @@
-#define TEST_DBC "/Users/leoneltrich/CLionProjects/EyeCAN/dbcppp/tests/Test.dbc"
-constexpr const char* TEST_FILES_PATH = "/Users/leoneltrich/CLionProjects/EyeCAN/dbcppp/tests/test_files";
-#define TEST_DBC_WITH_SINGLE_COMMENTS "/Users/leoneltrich/CLionProjects/EyeCAN/dbcppp/tests/Test_single_comments.dbc"
